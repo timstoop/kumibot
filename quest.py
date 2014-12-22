241f11b3d31db0fcca3c391f7aef146194b5bc48
Tim Stoop <tim@kumina.nl>
2014-12-22 23:57:22 +0100

    Refactor logging and allow user login/logout.

--- quest.py
+++ quest.py
@@ -1,46 +1,50 @@
 import os.path
 import cPickle
+import logging
 
 
 class Quest:
-    def __init__(self, logger):
-        self.users = []
-        self.usernames_in_use = []
-        self.logger = logger
+    def __init__(self):
+        self.users = {}
 
     def create_user(self, username):
-        if username not in self.usernames_in_use:
-            self.users.append(QuestUser(self.logger, username))
-            self.usernames_in_use.append(username)
+        if username not in self.users:
+            self.users[username] = QuestUser(username)
+
+    def hibernate_user(self, username):
+        if username in self.users:
+            self.users[username].hibernate()
+            del self.users[username]
 
 
 class QuestUser:
-    def __init__(self, logger, username):
+    def __init__(self, username):
         self.version = 1
         self.username = username
-        self.logger = logger
         if os.path.exists("archive/" + self.username + ".user"):
-            self.logger.log("User '%s' found in archive." % self.username)
+            logging.info("User '%s' found in archive." % self.username)
             self.load()
         else:
-            self.logger.log("User '%s' not found in archive." % self.username)
+            logging.info("User '%s' not found in archive." % self.username)
             self.save()
 
+    def hibernate(self):
+        logging.info("User '%s' goes into hibernation." % (self.username))
+        self.save()
+
     def load(self):
-        f = open('archive/' + self.username + '.user')
-        tmp_dict = cPickle.load(f)
-        f.close()
+        with open('archive/' + self.username + '.user') as f:
+            tmp_dict = cPickle.load(f)
 
         loaded_version = tmp_dict['version']
-        self.logger.log(" - Loaded userfile with data format version %i." %
-                        loaded_version)
+        logging.info(" - Loaded userfile with data format version %i." %
+                     loaded_version)
 
         self.__dict__.update(tmp_dict)
 
     def save(self):
-        f = open('archive/' + self.username + '.user', 'wb')
-        cPickle.dump(self.__dict__, f, 2)
-        f.close()
+        with open('archive/' + self.username + '.user', 'wb') as f:
+            cPickle.dump(self.__dict__, f, 2)
 
-        self.logger.log("Saved user file for '%s', data format version %i." %
-                        (self.username, self.version))
+        logging.info("Saved user file for '%s', data format version %i." %
+                     (self.username, self.version))
--- questbot.py
+++ questbot.py
@@ -9,26 +9,7 @@ from quest import Quest
 # system imports
 import time
 import sys
-
-
-class MessageLogger:
-    """
-    An independent logger class (because separation of application
-    and protocol logic is a good thing).
-    """
-    def __init__(self, file):
-        self.file = file
-
-    def log(self, message):
-        """Write a message to the file."""
-        timestamp = time.strftime("[%H:%M:%S]", time.localtime(time.time()))
-        msg = '%s %s\n' % (timestamp, message)
-        self.file.write(msg)
-        self.file.flush()
-        log.msg(message)
-
-    def close(self):
-        self.file.close()
+import logging
 
 
 class QuestBot(irc.IRCClient):
@@ -39,15 +20,13 @@ class QuestBot(irc.IRCClient):
 
     def connectionMade(self):
         irc.IRCClient.connectionMade(self)
-        self.logger = MessageLogger(open(self.factory.filename, "a"))
-        self.logger.log("[connected at %s]" %
-                        time.asctime(time.localtime(time.time())))
+        log.msg("[connected at %s]" %
+                time.asctime(time.localtime(time.time())))
 
     def connectionLost(self, reason):
         irc.IRCClient.connectionLost(self, reason)
-        self.logger.log("[disconnected at %s]" %
-                        time.asctime(time.localtime(time.time())))
-        self.logger.close()
+        log.msg("[disconnected at %s]" %
+                time.asctime(time.localtime(time.time())))
 
     # callbacks for events
 
@@ -57,7 +36,7 @@ class QuestBot(irc.IRCClient):
 
     def joined(self, channel):
         """This will get called when the bot joins the channel."""
-        self.logger.log("[I have joined %s]" % channel)
+        log.msg("[I have joined %s]" % channel)
         self.names(channel).addCallback(self._log_channel_users).addCallback(
             self.init_quest, channel=channel)
 
@@ -67,23 +46,23 @@ class QuestBot(irc.IRCClient):
 
         # Check to see if they're sending me a private message
         if channel == self.nickname:
-            self.logger.log(">%s< %s" % (user, msg))
+            log.msg(">%s< %s" % (user, msg))
             self.handle_query(user, msg)
             # Make sure we return asap
-            self.logger.log(">%s< answer returned." % (self.nickname))
+            log.msg(">%s< answer returned." % (self.nickname))
             return
 
         # Otherwise check to see if it is a message directed at me
         if msg.startswith(self.nickname + ":"):
-            self.logger.log("<%s> %s" % (user, msg))
+            log.msg("<%s> %s" % (user, msg))
             reply = "%s: I am a log bot" % user
             self.msg(channel, reply)
-            self.logger.log("<%s> %s" % (self.nickname, reply))
+            log.msg("<%s> %s" % (self.nickname, reply))
 
     def action(self, user, channel, msg):
         """This will get called when the bot sees someone do an action."""
         user = user.split('!', 1)[0]
-        self.logger.log("* %s %s" % (user, msg))
+        log.msg("* %s %s" % (user, msg))
 
     # irc commands
 
@@ -103,7 +82,7 @@ class QuestBot(irc.IRCClient):
         """Called when an IRC user changes their nickname."""
         old_nick = prefix.split('!')[0]
         new_nick = params[0]
-        self.logger.log("%s is now known as %s" % (old_nick, new_nick))
+        log.msg("%s is now known as %s" % (old_nick, new_nick))
 
     def irc_RPL_NAMREPLY(self, prefix, params):
         channel = params[2].lower()
@@ -127,14 +106,22 @@ class QuestBot(irc.IRCClient):
 
         del self.channel[channel]['namecallback']
 
+    def userJoined(self, user, channel):
+        log.msg("User '%s' joined channel '%s'." % (user, channel))
+        self.quest.create_user(user)
+
+    def userLeft(self, user, channel):
+        log.msg("User '%s' left channel '%s'." % (user, channel))
+        self.quest.hibernate_user(user)
+
     # Bot functionality
 
     def init_quest(self, users, channel):
-        q = Quest(self.logger)
+        self.quest = Quest()
         for user in users:
-            if user == self.nickname:
+            if user == self.nickname or user == ("@%s" % (self.nickname)):
                 continue
-            q.create_user(user)
+            self.quest.create_user(user)
 
     def handle_query(self, user, msg):
         if msg.startswith('help'):
@@ -147,10 +134,10 @@ class QuestBot(irc.IRCClient):
     # Helper functions
 
     def _log_error(self, msg):
-        self.logger.log("Something went wrong: %s" % msg)
+        log.msg("Something went wrong: %s" % msg)
 
     def _log_channel_users(self, users):
-        self.logger.log("Users in channel: %s" % users)
+        log.msg("Users in channel: %s" % users)
         return users
 
 
@@ -160,9 +147,8 @@ class QuestBotFactory(protocol.ClientFactory):
     A new protocol instance will be created each time we connect to the server.
     """
 
-    def __init__(self, channel, filename):
+    def __init__(self, channel):
         self.channel = channel
-        self.filename = filename
 
     def buildProtocol(self, addr):
         p = QuestBot()
@@ -179,11 +165,28 @@ class QuestBotFactory(protocol.ClientFactory):
 
 
 if __name__ == '__main__':
-    # initialize logging
-    log.startLogging(sys.stdout)
+    channel = sys.argv[1]
+    logfile = sys.argv[2]
+    # initialize logging, we use the default logging module, but want to allow
+    # twisted to write there as well. Found the solution on their own page:
+    # http://twistedmatrix.com/documents/current/core/howto/logging.html
+    observer = log.PythonLoggingObserver()
+    observer.start()
+    formatstring = '%(asctime)s %(name)-12s %(levelname)-8s %(message)s'
+    logging.basicConfig(level=logging.DEBUG,
+                        format=formatstring,
+                        datefmt='%m-%d %H:%M',
+                        filename=logfile,
+                        filemode='w')
+    # We also want to log directly to the console
+    console = logging.StreamHandler()
+    formatter = logging.Formatter(formatstring)
+    console.setFormatter(formatter)
+    # add the handler to the root logger
+    logging.getLogger('').addHandler(console)
 
     # create factory protocol and application
-    f = QuestBotFactory(sys.argv[1], sys.argv[2])
+    f = QuestBotFactory(channel)
 
     # connect factory to this host and port
     reactor.connectTCP("irc.kumina.nl", 6667, f)
